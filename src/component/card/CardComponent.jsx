07818a2b7ce1f97cce54d7051555a8dd058e8b1b
Filmm <49941017+Filmm@users.noreply.github.com>
2021-02-07 16:33:43 +0700

    update layout card

--- src/component/card/CardComponent.jsx
+++ src/component/card/CardComponent.jsx
@@ -1,57 +1,75 @@
 import React from 'react';
-import { Progress, Grid, Header, Image, Segment, Icon, Divider } from 'semantic-ui-react';
+import { Progress, Card, Header, Image, Segment, Icon, Message, Table, Grid } from 'semantic-ui-react';
 import { mockData } from "./mockData";
 
 class CardComponent extends React.Component {
     state = {
-        id: ''
+
     }
 
-    renderProgressBar = (header, value) => {
-        return <>
-            {header}
-            <div className="div--progress">
-                <div className="progress" style={{ width: `${value}%`, height: '13px' }}>
-                </div>
-            </div>
-        </>
+    renderMessageContent =() => {
+
     }
 
     render() {
         return (
             <>
-                {mockData.data.map((item, index) =>
-                    <Grid.Column>
-                        <Segment className="segment--card">
-                            <Icon name="close" className="close--icon cursor--pointer" />
+            {mockData.data.map((item, index) =>
+            <Grid.Column>
+                <Segment>
+                    <Grid>
+                        <Grid.Column width={6}>
+                            <Image src={`https://source.unsplash.com/random/160x160?/monster=${index}`}  />
+                        </Grid.Column>
 
-                            <Grid className="remove--boxShadow">
-                                <Grid.Column width={5} className="padding--0 remove--boxShadow">
-                                    <Image src={`https://source.unsplash.com/random?sig=${index}`} />
-                                </Grid.Column>
+                        <Grid.Column width={10}>
+                            <Table basic='very'>
+                                <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell width={7}>{item.name}</Table.HeaderCell>
+                                        <Table.HeaderCell textAlign="right" width={9}><Icon name="close" link/></Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Header>
 
-                                <Grid.Column >
-                                    <Grid columns={2}>
-                                        <Grid.Row>
-                                            hi
-                                    </Grid.Row>
+                                <Table.Body>
+                                    <Table.Row>
+                                        <Table.Cell>{item.hp.toUpperCase()}</Table.Cell>
+                                        <Table.Cell>
+                                        <div className="div--progress">
+                                            <div className="progress" style={{ width: `${item.hp_value}%`, height: '13px' }}></div>
+                                        </div>
+                                        </Table.Cell>
+                                    </Table.Row>
 
-                                        <Grid.Row>
+                                    <Table.Row>
+                                        <Table.Cell>{item.str.toUpperCase()}</Table.Cell>
+                                        <Table.Cell>
+                                        <div className="div--progress">
+                                            <div className="progress" style={{ width: `${item.str_value}%`, height: '13px' }}></div>
+                                        </div>
+                                        </Table.Cell>
+                                    </Table.Row>
 
-                                            {this.renderProgressBar('hi', '80')}
-                                        </Grid.Row>
-                                    </Grid>
+                                    <Table.Row>
+                                        <Table.Cell>{item.weak.toUpperCase()}</Table.Cell>
+                                        <Table.Cell>
+                                        <div className="div--progress">
+                                            <div className="progress" style={{ width: `${item.weak_value}%`, height: '13px' }}></div>
+                                        </div>
+                                        </Table.Cell>
+                                    </Table.Row>
 
+                                    <Table.Row>
+                                        <Table.Cell><Icon name="heart" color="red"/><Icon name="heart" color="red"/><Icon name="heart" color="red"/></Table.Cell>
 
-
-                                </Grid.Column>
-
-
-
-                            </Grid>
-                        </Segment>
-                    </Grid.Column>
-                )}
+                                    </Table.Row>
+                                </Table.Body>
+                                </Table>
+                        </Grid.Column>
+                    </Grid>
+                </Segment>
+            </Grid.Column>
+            )}
             </>
         )
     }
